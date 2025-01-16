3014652f37369901005c9dd5c3a5c4733adb1d89
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2025-01-17 01:50:56 +0700

    feat: create short url page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,71 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { FormEvent, useState } from "react";
+import { AxiosResponse } from "axios";
+import { apiInstance } from "../../libs/axios";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+interface CreatedUrl {
+  id: number
+  originalUrl: string
+  shortUrl: string
+}
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export default function Index() {
+  const [createdUrls, setCreatedUrls] = useState<CreatedUrl[]>([])
 
-export default function Home() {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    
+    const form = e.target as HTMLFormElement;
+    const originalUrl = (form.elements.namedItem('originalurl') as HTMLInputElement).value;
+    
+    try {
+      const res: AxiosResponse<{ data: CreatedUrl }> = await apiInstance.post('/urls', null, {
+        params: {
+          path: originalUrl
+        }
+      })
+      const createdUrl: CreatedUrl = {
+        id: res.data.data.id,
+        originalUrl: res.data.data.originalUrl,
+        shortUrl: res.data.data.shortUrl,
+      }
+      setCreatedUrls((prevItems) => [createdUrl, ...prevItems])
+    } catch (error) {
+      console.error(error)
+    }
+
+    form.reset()
+  }
+
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="container mx-auto max-w-screen-lg flex flex-col justify-center min-h-screen relative">
+      <form onSubmit={handleSubmit} className="-mt-72 flex flex-col absolute w-full">
+        <label htmlFor="originalurl" className="text-6xl text-center">Shorten a long URL</label>
+        <div className="flex mt-24">
+          <Input type="text" id="originalurl" name="originalurl" placeholder="Enter your link here" className="rounded-r-none h-12 md:text-lg" />
+          <Button type="submit" className="rounded-l-none h-12 text-md bg-green-600 hover:bg-green-700">Shorten URL</Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </form>
+      <div className="mt-8 flex flex-col w-[30rem] gap-6 absolute ml-auto mr-auto left-0 right-0 top-[480px]">
+        {createdUrls.map(url => {
+          return (
+            <div key={url.id} className="bg-gray-200 rounded-lg px-4 py-6">
+              <div>
+                <p>Your Long URL</p>
+                <Input value={url.originalUrl} readOnly className="bg-white mt-1 h-10" />
+              </div>
+              <div className="mt-5">
+                <p>Your Short URL</p>
+                <div className="flex items-center bg-red-100 mt-1 h-10">
+                  <Input value={url.shortUrl} readOnly className="bg-white h-full rounded-r-none" />
+                  <Button type="submit" className="rounded-l-none h-full bg-green-600 hover:bg-green-700">Copy</Button>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
